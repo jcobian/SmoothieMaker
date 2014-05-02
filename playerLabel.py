867c4d8b577eeb6176cafc536751c908cdb71b04
Oliver Lamb <Oliver@oliverlambsmbp.nomad.nd.edu>
2014-05-02 17:55:47 -0400

    updated playerlabel

--- playerLabel.py
+++ playerLabel.py
@@ -6,7 +6,7 @@ class PlayerLabel(pygame.sprite.Sprite):
 			self.white = 255,255,255
 			self.myfont = pygame.font.SysFont("monospace", size)
 			self.label = self.myfont.render(textLabel,1,self.white)
-			self.rect.center = (xpos, ypos)
+			self.rect = (xpos, ypos)
 			
 	def tick(self):
 		pass
