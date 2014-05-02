1406c36e2d8828e09a4dc7a118443fb5ec43a0a6
jcobian <jcobian@nd.edu>
2014-05-02 16:36:14 -0400

    typo

--- client.py
+++ client.py
@@ -29,13 +29,14 @@ class CommandConn(protocol.Protocol):
 			lc = LoopingCall(self.gs.gameLoopIteration)
 			lc.start(1/60)
 			pd = pickle.dumps(self.gs.blender.rect)
-			self.transport.write(self.playerNumber+":"+pd)
+			self.transport.write(str(self.playerNumber)+":"+pd)
 		else:
+				print 'got a pickle!'
 				pd = data
 				opponent = pickle.loads(pd)
 				self.gs.updateOpponent(opponent)
 				pd = pickle.dumps(self.gs.blender.rect)
-				self.transport.write(self.playerNumber+":"+pd)
+				self.transport.write(str(self.playerNumber)+":"+pd)
 		'''
 		elif data.startswith('Request'):
 			comp = data.split(':')
