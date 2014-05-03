2c876bfdf3071d2d71bf502218b4346f1b8ac421
jcobian <jcobian@nd.edu>
2014-05-03 17:00:58 -0400

    adding two ports

--- client.py
+++ client.py
@@ -5,53 +5,40 @@ import pickle
 from smoothie import GameSpace
 from twisted.internet.defer import DeferredQueue
 class Client():
-	def __init__(self,host,port):
+	def __init__(self,host,fruitPort,blenderPort):
 		self.host = host
-		self.port = port
-		reactor.connectTCP(self.host,self.port,CommandConnFactory(self))
+		self.fruitPort = fruitPort
+		self.blenderPort = blenderPort
+		reactor.connectTCP(self.host,self.blenderPort,BlenderConnFactory(self))
+		reactor.connectTCP(self.host,self.fruitPort,FruitConnFactory(self))
 		reactor.run()
 
 
-class CommandConn(protocol.Protocol):
+class FruitConn(protocol.Protocol):
 	def __init__(self,client):
-		self.numMessagesReceived = 0
 		self.client = client
 		self.fruitQueue = DeferredQueue()
 		self.fruitQueue.get().addCallback(self.sendMyData)
 
 
 	def sendMyData(self,fruitData):
-		pd = pickle.dumps(self.gs.blender.rect)
-		score = self.gs.score
 		datapd =  pickle.dumps(fruitData)
-		'''
-		randFruitInt = fruitData.fruitInt
-		randXPos = fruitData.xpos
-		foodType = fruitData.fruitType
-		randVSpeed = fruitData.vspeed
-		'''
-		'''
-		randFruitInt = self.gs.randFruitInt
-		randXPos = self.gs.randXPos
-		foodType = self.gs.foodType
-		randVSpeed = self.gs.randVSpeed
-		'''
-		theString = str(self.playerNumber)+':'+pd+':'+str(score)+':'+datapd
-		#theString= str(self.playerNumber)+':'+pd+':'+str(score)+':'+str(randFruitInt)+':'+str(randXPos)+':'+str(randVSpeed)+':'+foodType+'end'
+		theString = str(self.playerNumber)+':'+datapd
 		comp = theString.split(':')
 		self.transport.write(theString)
 
 	def readyForMore(self):
 		self.fruitQueue.get().addCallback(self.sendMyData)
-
-
+	
 		
-		
-	def closeConn(self):
-		reactor.stop()
 	def connectionMade(self):
 		self.transport.write('connect')
+
+	def closeConn(self):
+		reactor.stop()
+
 	def dataReceived(self,data):
+
 		if data == 'waiting for players':
 			print 'Waiting for another player..'
 		elif data == 'lost conn':
@@ -65,14 +52,50 @@ class CommandConn(protocol.Protocol):
 			self.gs.main()
 			self.lc = LoopingCall(self.gs.gameLoopIteration)
 			self.lc.start(1/60)
-			#IS THIS NEEDED
-			#self.sendMyData()
+
 		elif data == 'ready for more':
 			self.readyForMore()
 			
 		else:
 			self.parseData(data)
-			#self.sendMyData()
+
+	def parseData(self,data):
+		comp = data.split(':')
+		fruitData = pickle.loads(comp[1])
+		self.gs.addFruit(fruitData.fruitInt,fruitData.xpos,fruitData.vspeed,fruitData.foodType)
+		self.transport.write('added fruit:'+str(self.playerNumber))
+
+			 
+	def gameOver(self,text):
+		self.lc = LoopingCall(self.gs.goToGameOver,(text))
+		self.lc.start(1/60)
+
+
+
+class BlenderConn(protocol.Protocol):
+	def __init__(self,client):
+		self.client = client
+
+
+	def sendMyData(self,fruitData):
+		pd = pickle.dumps(self.gs.blender.rect)
+		score = self.gs.score
+		
+		theString = str(self.playerNumber)+':'+pd+':'+str(score)
+		comp = theString.split(':')
+		self.transport.write(theString)
+
+
+
+		
+		
+	def closeConn(self):
+		reactor.stop()
+
+	def dataReceived(self,data):
+		self.parseData(data)
+		self.sendMyData()
+
 	def parseData(self,data):
 		comp = data.split(':')
 		pd = comp[1]
@@ -80,53 +103,32 @@ class CommandConn(protocol.Protocol):
 		self.gs.updateOpponent(opponent)
 		oppScore = int(comp[2])
 		self.gs.opponentScore = oppScore
-		fruitData = pickle.loads(comp[3])
-		self.gs.addFruit(fruitData.fruitInt,fruitData.xpos,fruitData.vspeed,fruitData.foodType)
-		self.transport.write('added fruit:'+str(self.playerNumber))
-
-
-		'''
-		splitUp = data.split('end')
-		for piece in splitUp:
-			if len(piece)==0:
-				continue
-			comp = piece.split(':')
-			pd = comp[1]
-			opponent = pickle.loads(pd)
-			self.gs.updateOpponent(opponent)
-			oppScore = int(comp[2])
-			self.gs.opponentScore = oppScore
-			valid = int(comp[3])
-			print 'valid is ',valid
-			if valid == 1:
-				fruitInt = int(comp[4])
-				xpos = int(comp[5])
-				vspeed = int(comp[6])
-				foodType = comp[7]
-				print self.playerNumber,'is adding:',fruitInt,xpos,vspeed,foodType
-				self.gs.addFruit(fruitInt,xpos,vspeed,foodType)
-		'''
+		
 
 			 
 		
 		
-	def gameOver(self,text):
-		self.lc = LoopingCall(self.gs.goToGameOver,(text))
-		self.lc.start(1/60)
 
 	
 
 
-class CommandConnFactory(protocol.ClientFactory):
+class FruitConnFactory(protocol.ClientFactory):
 	def __init__(self,client):
 		self.client = client
 	def buildProtocol(self,addr):
-		return CommandConn(self.client)
+		return FruitConn(self.client)
+
+class BlenderConnFactory(protocol.ClientFactory):
+	def __init__(self,client):
+		self.client = client
+	def buildProtocol(self,addr):
+		return BlenderConn(self.client)
 
 if __name__ == '__main__':
 	host = sys.argv[1]
-	port = int(sys.argv[2])
-	client = Client(host,port)
+	fruitPort = int(sys.argv[2])
+	blenderPort = int(sys.argv[3])
+	client = Client(host,fruitPort,blenderPort)
 
 
 
