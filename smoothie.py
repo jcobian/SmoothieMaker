4f6a079a7320618641c3b7928175e29182a77bd7
jcobian <jcobian@nd.edu>
2014-05-03 12:39:12 -0400

    dbegu

--- smoothie.py
+++ smoothie.py
@@ -92,7 +92,7 @@ class GameSpace:
 				xpos = random.randint(0,self.width/2)
 				vspeed = random.randint(3,6)
 				fruit = Fruit(self,type='fruit',xpos=xpos,randFruitInt=randFruitInt,vspeed=vspeed)
-				print fruit.rect.top
+				print 'created',fruit.rect.top
 				xpos += self.width/2
 				fruit2 = Fruit(self,type='fruit',xpos=xpos,randFruitInt=randFruitInt,vspeed=vspeed)
 				self.fruits.append(fruit)
@@ -181,13 +181,12 @@ class GameSpace:
 			imageName = fd['image']
 			frozen = fd['frozen']
 			rect = pickle.loads(fd['rect'])
-			print rect.top
+			print 'in update',rect.top
 			vspeed = fd['vspeed']
 			currentTicks = fd['currentTicks']
 			fruit = Fruit(gs=self)
 			fruit.updateFruit(imageName,frozen,rect,vspeed,currentTicks)
 			self.fruitsOpp.append(fruit)
-			self.commandConn.lc.stop()
 			self.commandConn.closeConn()
 
 
