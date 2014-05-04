03eaace56ec5888231e28bf2e75e1c392cc4cef3
jcobian <jcobian@nd.edu>
2014-05-04 00:32:14 -0400

    pls god

--- fruitdata.py
+++ fruitdata.py
@@ -1,15 +1,16 @@
 class FruitData():
 	def __init__(self,fruitInt=0,xpos=0,vspeed=0,foodType='fruit',fruitID=0,freezeDirection='',freezeID='',dataType='create'):
+		self.dataType = dataType
 		if dataType == 'create':
 			self.fruitInt = fruitInt
 			self.xpos = xpos
 			self.vspeed = vspeed
 			self.foodType = foodType
 			self.fruitID = fruitID
-			self.dataType = dataType
 		elif dataType=='freeze':
 			self.freezeDirection = freezeDirection
 			self.freezeID = freezeID
+
 
 	def toString(self):
 			return str(self.fruitInt) + ":" + str(self.xpos) + ":" + str(self.vspeed) + ":" + self.foodType + ":" + str(self.fruitID) + ":" + self.freezeString
