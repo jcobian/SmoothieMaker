1e8dbd9fd5bae7033bc6af6e6c4c692a28b0d9f4
jcobian <jcobian@nd.edu>
2014-05-03 20:18:17 -0400

    spelled raspberry wrong

--- smoothie.py
+++ smoothie.py
@@ -13,9 +13,9 @@ from fruitdata import FruitData
 class GameSpace:
 	def __init__(self,commandConn,playerNumber,randSeed):
 		self.commandConn = commandConn
-		self.listOfFruitImages=['strawberry.png','banana.png','rasberry.png']
+		self.listOfFruitImages=['strawberry.png','banana.png','raspberry.png']
 		self.listOfVegetableImages = ['potato.png', 'onion.png']
-		self.listOfFrozenFruitImages = ['strawberryfrozen.png','bananafrozen.png','rasberryfrozen.png']
+		self.listOfFrozenFruitImages = ['strawberryfrozen.png','bananafrozen.png','raspberryfrozen.png']
 		self.listOfFrozenVegetableImages = ['potatofrozen.png', 'frozenonion.png']
 		self.playerNumber = playerNumber
 
